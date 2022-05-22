a0d156b9560c01889cffac60c2265c4fb669709e
antons-stepanovs <104780319+antons-stepanovs@users.noreply.github.com>
2022-05-22 21:09:34 +0300

    homework_update
    homework update

--- src/main/java/homework/FirstClass.java
+++ src/main/java/homework/FirstClass.java
@@ -3,5 +3,125 @@ package homework;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("This is homework");
+
+        String country = "Spain";
+        double population = 47.45;
+        int totalArea = 505990;
+        String capital = "Madrid";
+        String officialLanguage = "Spanish";
+        boolean ifinEU = true;
+        char currency ='€';
+
+
+        System.out.println("Country: " + country);
+        System.out.println("Population: " + population + " million");
+        System.out.println("Area: " + totalArea + " km2");
+        System.out.println("Capital: " + capital);
+        System.out.println("Official language: " + officialLanguage);
+        System.out.println("Member of the EU?: " + ifinEU);
+        System.out.println("Currency: " + currency);
+
+        System.out.println(" ");
+
+        System.out.println(country + "'s capital and largest city is " + capital+".");
+        System.out.println(country + "'s population exceeds " + population + " million" + ".");
+        System.out.println(officialLanguage + " is the world's second-most spoken native language.");
+        System.out.println("With an area of " + totalArea + " " + country+ " is the largest country in Southern Europe.");
+
+        System.out.println(" ");
+
+        int a = 450;
+        int b = 200;
+        int c = 150;
+        double d = 50.5;
+
+        int rezultats1;
+        rezultats1 = a + b;
+        System.out.println(a + " + " + b + " = " + rezultats1);
+
+        System.out.println(" ");
+
+        int rezultats2;
+        rezultats2 = b + c;
+        System.out.println(b + " + " + c + " = " + rezultats2);
+
+        System.out.println(" ");
+
+        double rezultats3;
+        rezultats3 = c + d;
+        System.out.println(c + " + " + d + " = " + rezultats3);
+
+        System.out.println(" ");
+
+        int rezultats4;
+        rezultats4 = a * b;
+        System.out.println(a + " * " + b + " = " + rezultats4);
+
+        System.out.println(" ");
+
+        int rezultats5;
+        rezultats5 = b * c;
+        System.out.println(b + " * " + c + " = " + rezultats5);
+
+        System.out.println(" ");
+
+        double rezultats6;
+        rezultats6 = c * d;
+        System.out.println(c + " * " + d + " = " + rezultats6);
+
+        System.out.println(" ");
+
+        int rezultats7;
+        rezultats4 = a - b;
+        System.out.println(a + " - " + b + " = " + rezultats4);
+
+        System.out.println(" ");
+
+        int rezultats8;
+        rezultats8 = b - c;
+        System.out.println(b + " - " + c + " = " + rezultats8);
+
+        System.out.println(" ");
+
+        double rezultats9;
+        rezultats9 = c - d;
+        System.out.println(c + " - " + d + " = " + rezultats9);
+
+        System.out.println(" ");
+
+        double rezultats10;
+        rezultats10 = a / b;
+        System.out.println(a + " / " + b + " = " + rezultats10);
+
+        System.out.println(" ");
+
+        double rezultats11;
+        rezultats11 = b / c;
+        System.out.println(b + " / " + c + " = " + rezultats11);
+
+        System.out.println(" ");
+
+        double rezultats12;
+        rezultats12 = c / d;
+        System.out.println(c + " / " + d + " = " + rezultats12);
+
+        System.out.println(" ");
+
+        double rezultats13;
+        rezultats13 = a % b;
+        System.out.println(a + " % " + b + " = " + rezultats13);
+
+        System.out.println(" ");
+
+        double rezultats14;
+        rezultats14 = b % c;
+        System.out.println(b + " % " + c + " = " + rezultats14);
+
+        System.out.println(" ");
+
+        double rezultats15;
+        rezultats15 = c % d;
+        System.out.println(c + " % " + d + " = " + rezultats15);
+
     }
 }
