d28f9007a0e349aebf249f042c280310ff9576e0
antons-stepanovs <104780319+antons-stepanovs@users.noreply.github.com>
2022-06-15 22:42:23 +0300

    Update
    Update

--- src/main/java/homework/classHomework/Main.java
+++ src/main/java/homework/classHomework/Main.java
@@ -6,13 +6,14 @@ public class Main {
 
 
         Triangle triangle1 = new Triangle();
-        triangle1.a = 3;
-        triangle1.b = 3;
-        triangle1.c = 3;
+        triangle1.a = 17;
+        triangle1.b = 39;
+        triangle1.c = 44;
         System.out.println("Triangle area = " + triangle1.Area());
+        //System.out.println("Triangle area = " + triangle1.area);
         //System.out.println(triangle1.a + " " + triangle1.b + " " + triangle1.c);
         System.out.println("If triangle is Equilateral: " + triangle1.TriangleEquilateral());
-        System.out.println("If triangle is Equilateral: " + triangle1.TriangleIsosceles());
+        System.out.println("If triangle is Isosceles: " + triangle1.TriangleIsosceles());
 
 
         System.out.println("\n");
